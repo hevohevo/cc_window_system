6213e22998f6045b3c18e77c6cea1f53c9956ac2
hevohevo <hevohevo@gmail.com>
2014-08-06 20:25:06 +0900

    標準のWindow APIの仕様を参考としてメモ

---
+++ winsys.lua
@@ -0,0 +1,29 @@
+-- #################################################
+-- PockeComputer Windows System in ComputerCraft
+-- version 0.1
+-- http://hevohevo.hatenablog.com/
+
+
+-- 標準APIの仕様をメモ。http://computercraft.info/wiki/Term_(API) 　より
+-- window.create(親term, xPos, yPos, width, height, visible_flag)
+-- ex.
+-- local win = window.create(term.current(), 1, 1, 20, 10, true)
+-- win.write(text)
+-- win.clear()
+-- win.clearLine()
+-- win.getCursorPos()
+-- win.setCursorPos()
+-- win.setCurosrBlink()
+-- win.isColor()
+-- win.getSize()
+-- win.scroll()
+-- win.setTextColor()
+-- win.setBackgroundColor()
+-- win.setVisible( boolean_visivility )
+-- win.redraw()
+-- win.restoreCursor()
+-- win.getPosition()
+-- win.reposition(xPox, yPox, optional_with, optional_height)
+
+
+
